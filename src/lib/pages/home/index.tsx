e370250bf2c198068cf3d9bbfda74501328f5365
legitbeep <glorytoharsh@gmail.com>
2023-12-11 15:42:38 +0530

    Fix - remove pnpm

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,17 +1,23 @@
 'use client';
-import { Flex } from '@chakra-ui/react';
+import { Flex, useEditable } from '@chakra-ui/react';
 import ImageEditor from '~/lib/components/ImageEditor/ImageEditor';
 
 import './home.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoginForm from '~/lib/components/auth/Login';
 import SignupForm from '~/lib/components/auth/Signup';
 
 const Home = () => {
   const [activeScreen, setActiveScreen] = useState('login');
   const [isLoggedIn, setIsLoggedIn] = useState(
-    JSON.parse(localStorage.getItem('isLoggedIn') || 'false')
+    JSON.parse(localStorage?.getItem('isLoggedIn') || 'false')
   );
+
+  useEffect(() => {
+    setIsLoggedIn(
+      JSON.parse(localStorage?.getItem('isLoggedIn') || 'false')
+    )
+  },[])
   return (
     <Flex
       direction="column"
